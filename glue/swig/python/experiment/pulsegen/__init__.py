ac85a7a6ba88c24702120c1f420c08afed5217d3
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2010-10-31 19:01:14 +0000

    1. Defined stubs for the PulseGen abstract data type.

--- glue/swig/python/experiment/pulsegen/__init__.py
+++ glue/swig/python/experiment/pulsegen/__init__.py
@@ -27,3 +27,43 @@ class PulseGen:
         pass
 
 
+    def SetLevel1(self,level1):
+        pass
+
+    def SetWidth1(self,width1):
+        pass
+
+    def SetDelay1(self,delay1):
+        pass
+
+    def SetLevel2(self,level2):
+        pass
+
+    def SetWidth2(self,width2):
+        pass
+
+    def SetDelay2(self,delay2):
+        pass
+
+    def SetBaseLevel(self,base_level):
+        pass
+
+    def SetTriggerMode(self,trigger_mode):
+        pass
+
+    def SingleStep(self):
+        pass
+
+    def Reset(self):
+        pass
+
+    def Finish(self):
+        pass
+
+    def AddInput(self,input):
+        pass
+
+    def AddVariable(self,variable):
+        pass
+
+    
