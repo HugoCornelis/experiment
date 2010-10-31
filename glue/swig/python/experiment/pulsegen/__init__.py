909415e75c36c00d40df62f8820250625fb4318b
mandorodriguez+MacPro <mandorodriguez+MacPro@gmail.com>
2010-10-31 08:10:43 +0000

    1. Added some comments and documentation to the module.

--- glue/swig/python/experiment/pulsegen/__init__.py
+++ glue/swig/python/experiment/pulsegen/__init__.py
@@ -1,8 +1,18 @@
+"""!
+This is the module used to create a pulsegen python object
+from the swig bindings.
+"""
 import os
 import sys
 import pdb
 
 sys.path.append( os.path.join( os.getcwd(), '.' ) )
+
+__author__ = 'Mando Rodriguez'
+__credits__ = []
+__license__ = "GPL"
+__version__ = "0.1"
+__status__ = "Development"
 
 try:
 
