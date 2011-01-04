b8d6a73a894c7af19b46b0434316d093a957d8af
mandorodriguez <mandorodriguez@gmail.com>
2011-01-04 22:56:58 +0000

    1. Copied over the old python swig bindings to glue/swig/python_old. Build is still working and active.

---
+++ glue/swig/python/experiment/output.py
@@ -0,0 +1,24 @@
+
+import os
+import pdb
+import sys
+sys.path.append('/usr/local/glue/swig/python')
+import SwiggableExperiment
+
+
+
+class Output:
+    def __init__(self, filename):
+        self.backend = SwiggableExperiment.OutputGeneratorNew(filename)
+
+    def advance(self, time):
+        return SwiggableExperiment.OutputGeneratorTimedStep(self.backend, time)
+
+    def compile(self):
+        return SwiggableExperiment.OutputGeneratorInitiate(self.backend)
+
+    def AddOutput(self, name, address):
+        SwiggableExperiment.OutputGeneratorAddVariable(self.backend, name, address)
+
+    def finish(self):
+        SwiggableExperiment.OutputGeneratorFinish(self.backend)
