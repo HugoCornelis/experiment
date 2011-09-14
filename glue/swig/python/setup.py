6a27cd438d421be810beceabfaf5cc83f7a7adb8
mandorodriguez <mandorodriguez@gmail.com>
2011-09-14 18:27:05 +0000

    1. Changed the URL of the setup.py script to point to the pypi page for the experiment module.

--- glue/swig/python/setup.py
+++ glue/swig/python/setup.py
@@ -394,8 +394,8 @@ VERSION = _package_info.GetVersion()
 AUTHOR = cbi.__author__
 AUTHOR_EMAIL = cbi.__email__
 LICENSE = cbi.__license__
-URL = cbi.__url__
-DOWNLOAD_URL = cbi.__download_url__
+URL = "http://pypi.python.org/pypi/experiment"
+DOWNLOAD_URL = "http://pypi.python.org/pypi/experiment"
 DESCRIPTION="The Experiment package delivers a library of stimulation and recording objects."
 LONG_DESCRIPTION=cbi.__description__
 
